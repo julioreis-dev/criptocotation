d88172f60cb6352beed0588f978e32d2c1f848fd
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-02-05 20:08:31 -0300

    Fix STATIC_ROOT in the settings.py

--- assetsAdmin/settings/settings.py
+++ assetsAdmin/settings/settings.py
@@ -101,7 +101,7 @@ LOGOUT_REDIRECT_URL = 'home:index'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
