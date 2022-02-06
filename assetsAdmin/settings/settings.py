7914856f5b44a53fd9b6a17f4abdfc421266414b
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-02-05 22:08:56 -0300

    improvements in settings.py

--- assetsAdmin/settings/settings.py
+++ assetsAdmin/settings/settings.py
@@ -104,12 +104,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -117,10 +111,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jrfirmino01@gmail.com'
-EMAIL_HOST_PASSWORD = '&N!Tk3YgFpA5V'
+EMAIL_HOST_USER = 'julreifir'
+EMAIL_HOST_PASSWORD = '*DYnMlU2kwfAgKRg'
 
 django_heroku.settings(locals())
