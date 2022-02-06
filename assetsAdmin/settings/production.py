2471bd9dff57f2a44d02236ccfa6e8e6ad8459d7
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-02-05 21:23:41 -0300

    Improvement in whole project

--- assetsAdmin/settings/production.py
+++ assetsAdmin/settings/production.py
@@ -1,9 +1,10 @@
+from assetsAdmin.settings.settings import *
 import dj_database_url
 
 
-DEBUG = True
-
 SECRET_KEY = 'django-insecure-0g49mcmm=9#bs$u%&o4_r8&np)4-_4s#c@k)1c6sg@o73f+*v!'
+
+DEBUG = True
 
 # Alterar para o IP do ambiente de produção quando houver.
 ALLOWED_HOSTS = ['*']
--- assetsAdmin/settings/settings.py
+++ assetsAdmin/settings/settings.py
@@ -101,14 +101,15 @@ LOGOUT_REDIRECT_URL = 'home:index'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
