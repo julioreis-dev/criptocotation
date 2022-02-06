fd9b3a9e0736ef0990c83e903df81170ca803bad
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-02-05 21:36:03 -0300

    Updated ALLOWED_HOSTS in production.py

--- assetsAdmin/settings/production.py
+++ assetsAdmin/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-0g49mcmm=9#bs$u%&o4_r8&np)4-_4s#c@k)1c6sg@o73f+*v!
 DEBUG = True
 
 # Alterar para o IP do ambiente de produção quando houver.
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['criptocoincotation.herokuapp.com']
 
 DATABASES = {
     'default': dj_database_url.config()
