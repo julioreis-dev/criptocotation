be1dd507bd938f894fd74fed17559bc089fbfef0
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-02-07 00:06:32 -0300

    improvements in querys/querys.py and views/views.py

--- assets/querys/querys.py
+++ assets/querys/querys.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail
+from django.core.mail import send_mail, mail_admins
 from datetime import datetime
 from django.utils import timezone
 from dataclasses import dataclass
@@ -26,18 +26,27 @@ class Recommendation:
     def emailbody(self, client, cotations_dict, status):
         hours, minutes = self.analisetimer(client)
         if hours != 0 or minutes >= client.timer:
-            send_mail(
-                f'Recommendation to {status} the cripto {client.coin}',
-                f'Dear {client.user_id.username},\nAccording your alert "{client.nameconfig}", '
-                f'We recommend to {status} {client.coin} cripto:\nCurrent price: ${cotations_dict[client.coin]}'
-                f'\nAlert point: ${client.sell if status=="sell" else client.buy}',
-                'jrfirmino01@gmail.com',
-                [client.user_id.email],
-                fail_silently=False,
-            )
-            client.emaildate = datetime.now(timezone.utc)
-            # client.emaildate = timezone.now()
-            client.save()
+            # implantar try aqui
+            try:
+                send_mail(
+                    f'Recommendation to {status} the cripto {client.coin}',
+                    f'Dear {client.user_id.username},\nAccording your alert "{client.nameconfig}", '
+                    f'We recommend to {status} {client.coin} cripto:\nCurrent price: ${cotations_dict[client.coin]}'
+                    f'\nAlert point: ${client.sell if status == "sell" else client.buy}',
+                    'jrfirmino01@gmail.com',
+                    [client.user_id.email],
+                    fail_silently=False,
+                )
+                client.emaildate = datetime.now(timezone.utc)
+                # client.emaildate = timezone.now()
+                client.save()
+            except:
+                mail_admins(
+                    'Email não enviado',
+                    f'Email referente ao {client.user_id.username} com o alerta {client.nameconfig} '
+                    f'não foi encaminhado.',
+                    fail_silently=False,
+                )
 
     def emailrecommendation(self):
         clientscripto = self.allclientes()
@@ -50,7 +59,6 @@ class Recommendation:
             elif client.buy >= current_cotations_dict[client.coin]:
                 threadinstances = self.emailbody(client, current_cotations_dict, 'buy')
             threads.append(threading.Thread(target=threadinstances, args=()))
-
 
         [thread.start() for thread in threads]
         [thread.join() for thread in threads]
--- assets/views/views.py
+++ assets/views/views.py
@@ -1,6 +1,3 @@
-# from django.shortcuts import render
-# import multiprocessing as mp
-# from gevent.pool import Pool
 from django.views.generic import TemplateView
 from assets.models.models_assets import Coincotationtable
 import requests
@@ -28,39 +25,3 @@ def call_api():
 
 class IndexTemplateView(TemplateView):
     template_name = 'home/index.html'
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['info'] = salutation()
-    #     return context
-
-
-# def return_price(keyvaluetuplas):
-#     addressapi = f'https://api.coinpaprika.com/v1/price-converter?base_currency_id={keyvaluetuplas[1]}' \
-#                  f'&quote_currency_id=usd-us-dollars&amount=1'
-#     response = requests.get(addressapi).json()
-#     price_coin = round(response["price"], 2)
-#     return Coincotationtable(description=keyvaluetuplas[0], price=price_coin)
-#
-#
-# def call_api2():
-#     """Função que consome a API que fornece as cotações atuais das criptocoins."""
-#
-#     coins = {'BTC': 'btc-bitcoin', 'LINK': 'link-chainlink', 'XRP': 'xrp-xrp', 'LTC': 'ltc-litecoin',
-#              'CHZ': 'chz-chiliz', 'ETH': 'eth-ethereum', 'DOGE': 'doge-dogecoin', 'ADA': 'ada-cardano'}
-#     pool = Pool(8)
-#     coinameslist = [(coin, coins[coin]) for coin in coins]
-#     coinsinstances = pool.map(return_price, coinameslist)
-#     Coincotationtable.objects.bulk_create(coinsinstances)
-#     print('Executado')
-#
-#
-# def call_api1():
-#     coins = {'BTC': 'btc-bitcoin', 'LINK': 'link-chainlink', 'XRP': 'xrp-xrp', 'LTC': 'ltc-litecoin',
-#              'CHZ': 'chz-chiliz', 'ETH': 'eth-ethereum', 'DOGE': 'doge-dogecoin', 'ADA': 'ada-cardano'}
-#     coinameslist = [(coin, coins[coin]) for coin in coins]
-#     x = []
-#     for cointuple in coinameslist:
-#         x.append(return_price(cointuple))
-#     Coincotationtable.objects.bulk_create(x)
-#     print('Executado!!!')
