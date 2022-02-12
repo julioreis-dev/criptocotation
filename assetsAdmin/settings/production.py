4215ddfe988b28deeeceeb3e7cae8e03e8f1b2d6
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-02-11 23:09:11 -0300

    update in production.py

--- assetsAdmin/settings/production.py
+++ assetsAdmin/settings/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 SECRET_KEY = 'django-insecure-0g49mcmm=9#bs$u%&o4_r8&np)4-_4s#c@k)1c6sg@o73f+*v!'
 
-DEBUG = True
+DEBUG = False
 
 # Alterar para o IP do ambiente de produção quando houver.
 ALLOWED_HOSTS = ['criptocoincotation.herokuapp.com']
